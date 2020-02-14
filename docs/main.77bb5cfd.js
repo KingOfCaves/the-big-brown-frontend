3040b9ba58a5ddf125e1505747cb814597baddaf
Nick Wills <37917347+KingOfCaves@users.noreply.github.com>
2020-02-14 11:30:17 -0500

    updating images to content folder, adding portrait image for fake staff,  adding quotes around nutured because that isn't a real word at all i dont even know, rerunning post

--- docs/main.77bb5cfd.js
+++ docs/main.77bb5cfd.js
@@ -122,7 +122,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\steak-flame.jpg":[["steak-flame.08b0a7c2.jpg","images/steak-flame.jpg"],"images/steak-flame.jpg"],"./..\\images\\burg.jpg":[["burg.5cf1437d.jpg","images/burg.jpg"],"images/burg.jpg"],"./..\\images\\steak-rosemary.jpg":[["steak-rosemary.8dea0278.jpg","images/steak-rosemary.jpg"],"images/steak-rosemary.jpg"],"./..\\images\\kabob.jpg":[["kabob.ec2fbe72.jpg","images/kabob.jpg"],"images/kabob.jpg"],"./..\\images\\fast-car.jpg":[["fast-car.0e5eb2c4.jpg","images/fast-car.jpg"],"images/fast-car.jpg"],"./..\\images\\fast-car-2.jpg":[["fast-car-2.82f8dd61.jpg","images/fast-car-2.jpg"],"images/fast-car-2.jpg"],"./..\\images\\city-air.jpg":[["city-air.b1fc09de.jpg","images/city-air.jpg"],"images/city-air.jpg"],"./..\\images\\cheeseburger-fries.jpg":[["cheeseburger-fries.72ce9caf.jpg","images/cheeseburger-fries.jpg"],"images/cheeseburger-fries.jpg"],"./..\\images\\chef-wallace.jpg":[["chef-wallace.85ee19c3.jpg","images/chef-wallace.jpg"],"images/chef-wallace.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\steak-flame.jpg":[["steak-flame.08b0a7c2.jpg","images/steak-flame.jpg"],"images/steak-flame.jpg"],"./..\\images\\burg.jpg":[["burg.5cf1437d.jpg","images/burg.jpg"],"images/burg.jpg"],"./..\\images\\steak-rosemary.jpg":[["steak-rosemary.8dea0278.jpg","images/steak-rosemary.jpg"],"images/steak-rosemary.jpg"],"./..\\images\\kabob.jpg":[["kabob.ec2fbe72.jpg","images/kabob.jpg"],"images/kabob.jpg"],"./..\\images\\fast-car.jpg":[["fast-car.0e5eb2c4.jpg","images/fast-car.jpg"],"images/fast-car.jpg"],"./..\\images\\fast-car-2.jpg":[["fast-car-2.82f8dd61.jpg","images/fast-car-2.jpg"],"images/fast-car-2.jpg"],"./..\\images\\city-air.jpg":[["city-air.b1fc09de.jpg","images/city-air.jpg"],"images/city-air.jpg"],"./..\\images\\cheeseburger-fries.jpg":[["cheeseburger-fries.72ce9caf.jpg","images/cheeseburger-fries.jpg"],"images/cheeseburger-fries.jpg"],"./..\\images\\chef-wallace.jpg":[["chef-wallace.85ee19c3.jpg","images/chef-wallace.jpg"],"images/chef-wallace.jpg"],"./..\\images\\chef-old-man-magnifico.jpg":[["chef-old-man-magnifico.34111cd0.jpg","images/chef-old-man-magnifico.jpg"],"images/chef-old-man-magnifico.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -150,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59496" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59954" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
